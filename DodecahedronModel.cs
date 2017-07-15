f96b85b992c68480b0e45ea194d5a124d5351d8d
James Parsley <jimmparsley@googlemail.com>
2017-07-15 10:34:57 +0200

    change list of dodecahedron vertices to show how the numbers are calculated

--- DodecahedronModel.cs
+++ DodecahedronModel.cs
@@ -4,65 +4,66 @@ using System.Collections.Generic;
 
 namespace Dodecahedroid
 {
-	public class DodecahedronModel
-	{
+    public class DodecahedronModel
+    {
+        private readonly static float GOLDEN_RATIO = (float)((1 + Math.Sqrt(5)) / 2);  // = 1.618f;
 
-        //static DodecahedronModel()
-        //{
-        //    ComputeVertices();
-        //}
+        // explicit vertices from http://mathworld.wolfram.com/RegularDodecahedron.html
+        // 2*Math.Cos(2*0*Math.PI/5) = 2*Math.Cos(0) = 2 = 1.236*GOLDEN_RATIO
+        // 2*Math.Sin(2*0*Math.PI/5) = 2*Math.Sin(0) = 0
 
-        private readonly static float GOLDEN_RATIO = 1.618f;
+        // 2*Math.Cos(2*1*Math.PI/5) = 0.618f = 0.382*GOLDEN_RATIO = GOLDEN_RATIO-1
+        // 2*Math.Sin(2*1*Math.PI/5) = 1.9021 = 1.1756f*GOLDEN_RATIO
 
-        //internal readonly static float[] vertices = {
-        //	0, 1/GOLDEN_RATIO, GOLDEN_RATIO,
-        //          0, -1/GOLDEN_RATIO, -GOLDEN_RATIO,
-        //          0, 1/GOLDEN_RATIO, -GOLDEN_RATIO,
-        //          0, -1/GOLDEN_RATIO, GOLDEN_RATIO,
+        // 2*Math.Cos(2*2*Math.PI/5) = -GOLDEN_RATIO
+        // 2*Math.Sin(2*2*Math.PI/5) = 1.1756f = 0.7266*GOLDEN_RATIO
 
-        //          GOLDEN_RATIO, 0, 1/GOLDEN_RATIO,
-        //          -GOLDEN_RATIO, 0, -1/GOLDEN_RATIO,
-        //          GOLDEN_RATIO, 0, -1/GOLDEN_RATIO,
-        //          -GOLDEN_RATIO, 0, 1/GOLDEN_RATIO,
+        // 2*Math.Cos(2*3*Math.PI/5) = -GOLDEN_RATIO
+        // 2*Math.Sin(2*3*Math.PI/5) = -1.1756f = -0.7266*GOLDEN_RATIO
 
-        //          1/GOLDEN_RATIO, GOLDEN_RATIO, 0,
-        //          -1/GOLDEN_RATIO, -GOLDEN_RATIO, 0,
-        //          1/GOLDEN_RATIO, -GOLDEN_RATIO, 0,
-        //          -1/GOLDEN_RATIO, GOLDEN_RATIO, 0,
+        // 2*Math.Cos(2*4*Math.PI/5) = 0.618f = 0.382*GOLDEN_RATIO = GOLDEN_RATIO-1
+        // 2*Math.Sin(2*4*Math.PI/5) = -1.9021 = -1.1756f*GOLDEN_RATIO
 
-        //          1, 1, 1,
-        //          1, 1, -1,
-        //          1, -1, 1,
-        //          1, -1, -1,
-        //          -1, 1, 1,
-        //          -1, 1, -1,
-        //          -1, -1, 1,
-        //          -1, -1, -1,
-        //      };
-        private readonly static float[] dodecahedron_vertices = {
-            1.236f, 0.0f, 1.618f, // 0
-            0.382f, 1.1754f, 1.618f, // 1
-            -1.0f, 0.7263f, 1.618f, // 2
-            -1.0f, -0.7263f, 1.618f, // 3
-            0.382f, -1.1754f, 1.618f, // 4
+        // 2*GOLDEN_RATIO*Math.Cos(2*0*Math.PI/5) = 2*GOLDEN_RATIO 
+        // 2*GOLDEN_RATIO*Math.Sin(2*0*Math.PI/5) = 0
 
-            2.0f, 0.0f, 0.3819f, // 5
-            0.618f, 1.902f, 0.3819f, // 6
-            -1.6179f, 1.1755f, 0.3819f, // 7
-            -1.6179f, -1.1755f, 0.3819f, // 8
-            0.618f, -1.902f, 0.3819f, // 9
+        // 2*GOLDEN_RATIO*Math.Cos(2*1*Math.PI/5) = 0.618f*GOLDEN_RATIO = (GOLDEN_RATIO-1)*GOLDEN_RATIO = 1
+        // 2*GOLDEN_RATIO*Math.Sin(2*1*Math.PI/5) = 1.9021*GOLDEN_RATIO = 1.1756f*GOLDEN_RATIO*GOLDEN_RATIO
 
-            -2f, 0.0f, -0.3819f, // 10
-            -0.618f, -1.9020f, -0.3819f, // 11
-            1.6179f, -1.1755f, -0.3819f, // 12
-            1.6179f, 1.1755f, -0.3819f, // 13
-            -0.618f, 1.902f, -0.3819f, // 14
+        // 2*GOLDEN_RATIO*Math.Cos(2*2*Math.PI/5) = -GOLDEN_RATIO*GOLDEN_RATIO
+        // 2*GOLDEN_RATIO*Math.Sin(2*2*Math.PI/5) = 1.1756f*GOLDEN_RATIO = 0.7266*GOLDEN_RATIO*GOLDEN_RATIO
 
-            -1.236f, 0.0f, -1.618f, // 15
-            -0.382f, -1.1754f, -1.618f, // 16
-            1.0f, -0.7263f, -1.618f, // 17
-            1.0f, 0.7263f, -1.618f, // 18
-            -0.382f, 1.1754f, -1.618f, // 19
+        // 2*GOLDEN_RATIO*Math.Cos(2*3*Math.PI/5) = -GOLDEN_RATIO*GOLDEN_RATIO
+        // 2*GOLDEN_RATIO*Math.Sin(2*3*Math.PI/5) = -1.1756f*GOLDEN_RATIO = -0.7266*GOLDEN_RATIO*GOLDEN_RATIO
+
+        // 2*GOLDEN_RATIO*Math.Cos(2*4*Math.PI/5) = 0.618f*GOLDEN_RATIO = (GOLDEN_RATIO-1)*GOLDEN_RATIO = 1
+        // 2*GOLDEN_RATIO*Math.Sin(2*4*Math.PI/5) = -1.9021*GOLDEN_RATIO = -1.1756f*GOLDEN_RATIO*GOLDEN_RATIO
+
+
+        private static readonly float[] explicitDodecahedronVertices = {
+            (float)(2*Math.Cos(2*0*Math.PI/5)), (float)(2*Math.Sin(2*0*Math.PI/5)), GOLDEN_RATIO+1, // 0
+            (float)(2*Math.Cos(2*1*Math.PI/5)), (float)(2*Math.Sin(2*1*Math.PI/5)), GOLDEN_RATIO+1, // 1
+            (float)(2*Math.Cos(2*2*Math.PI/5)), (float)(2*Math.Sin(2*2*Math.PI/5)), GOLDEN_RATIO+1, // 2
+            (float)(2*Math.Cos(2*3*Math.PI/5)), (float)(2*Math.Sin(2*3*Math.PI/5)), GOLDEN_RATIO+1, // 3
+            (float)(2*Math.Cos(2*4*Math.PI/5)), (float)(2*Math.Sin(2*4*Math.PI/5)), GOLDEN_RATIO+1, // 4
+
+            (float)(2*GOLDEN_RATIO*Math.Cos(2*0*Math.PI/5)), (float)(2*GOLDEN_RATIO*Math.Sin(2*0*Math.PI/5)), GOLDEN_RATIO-1, // 5
+            (float)(2*GOLDEN_RATIO*Math.Cos(2*1*Math.PI/5)), (float)(2*GOLDEN_RATIO*Math.Sin(2*1*Math.PI/5)), GOLDEN_RATIO-1, // 6
+            (float)(2*GOLDEN_RATIO*Math.Cos(2*2*Math.PI/5)), (float)(2*GOLDEN_RATIO*Math.Sin(2*2*Math.PI/5)), GOLDEN_RATIO-1, // 7
+            (float)(2*GOLDEN_RATIO*Math.Cos(2*3*Math.PI/5)), (float)(2*GOLDEN_RATIO*Math.Sin(2*3*Math.PI/5)), GOLDEN_RATIO-1, // 8
+            (float)(2*GOLDEN_RATIO*Math.Cos(2*4*Math.PI/5)), (float)(2*GOLDEN_RATIO*Math.Sin(2*4*Math.PI/5)), GOLDEN_RATIO-1, // 9
+
+            (float)(-2*GOLDEN_RATIO*Math.Cos(2*0*Math.PI/5)), (float)(-2*GOLDEN_RATIO*Math.Sin(2*0*Math.PI/5)), -(GOLDEN_RATIO-1), // 10
+            (float)(-2*GOLDEN_RATIO*Math.Cos(2*1*Math.PI/5)), (float)(-2*GOLDEN_RATIO*Math.Sin(2*1*Math.PI/5)), -(GOLDEN_RATIO-1), // 11
+            (float)(-2*GOLDEN_RATIO*Math.Cos(2*2*Math.PI/5)), (float)(-2*GOLDEN_RATIO*Math.Sin(2*2*Math.PI/5)), -(GOLDEN_RATIO-1), // 12
+            (float)(-2*GOLDEN_RATIO*Math.Cos(2*3*Math.PI/5)), (float)(-2*GOLDEN_RATIO*Math.Sin(2*3*Math.PI/5)), -(GOLDEN_RATIO-1), // 13
+            (float)(-2*GOLDEN_RATIO*Math.Cos(2*4*Math.PI/5)), (float)(-2*GOLDEN_RATIO*Math.Sin(2*4*Math.PI/5)), -(GOLDEN_RATIO-1), // 14
+
+            (float)(-2*Math.Cos(2*0*Math.PI/5)), (float)(-2*Math.Sin(2*0*Math.PI/5)), -(GOLDEN_RATIO+1), // 15
+            (float)(-2*Math.Cos(2*1*Math.PI/5)), (float)(-2*Math.Sin(2*1*Math.PI/5)), -(GOLDEN_RATIO+1), // 16
+            (float)(-2*Math.Cos(2*2*Math.PI/5)), (float)(-2*Math.Sin(2*2*Math.PI/5)), -(GOLDEN_RATIO+1), // 17
+            (float)(-2*Math.Cos(2*3*Math.PI/5)), (float)(-2*Math.Sin(2*3*Math.PI/5)), -(GOLDEN_RATIO+1), // 18
+            (float)(-2*Math.Cos(2*4*Math.PI/5)), (float)(-2*Math.Sin(2*4*Math.PI/5)), -(GOLDEN_RATIO+1), // 19
         };
 
         // for pentagon a b c d e
@@ -71,73 +72,12 @@ namespace Dodecahedroid
         // a d e
         // b c d
 
-        // order of specifying vertices affects direction of default normal
+        // order of specifying triangle vertices affects direction of default normal
 
-		//private static readonly ushort[] dodecahedron_faceIndexes = {
-  //          // one of pair of opposite pentagons parallel to xy plane
-  //          // 0, 1, 2, 3, 4
-  //          1, 0, 3,
-  //          0, 4, 3,
-  //          1, 3, 2,
-
-  //          // 0, 4, 5, 9, 12
-  //          0, 12, 4,
-  //          0, 5, 12,
-  //          4, 12, 9,
-
-  //          // 3, 4, 8, 9, 11
-  //          3, 4, 11,
-  //          3, 11, 8,
-  //          4, 9, 11,
-            
-  //          // 2, 3, 7, 8, 10
-  //          2, 3, 10,
-  //          2, 10, 7,
-  //          3, 8, 10,
-            
-  //          // 1, 2, 6, 7, 14
-  //          1, 2, 14,
-  //          1, 14, 6,
-  //          2, 7, 14,
-
-  //          // 0, 1, 5, 6, 13
-  //          0, 1, 13,
-  //          0, 13, 5,
-  //          1, 6, 13,
-
-
-  //          // 15, 16, 17, 18, 19
-  //          16, 18, 15,
-  //          15, 18, 19,
-  //          16, 17, 18,
-
-  //          // 15, 19, 10, 14, 7
-  //          15, 19, 7,
-  //          15, 7, 10,
-  //          19, 14, 7,
-
-  //          // 18, 19, 13, 14, 6
-  //          18, 6, 19,
-  //          18, 13, 6,
-  //          19, 6, 14,
-
-  //          // 17, 18, 12, 13, 5
-  //          17, 5, 18,
-  //          17, 12, 5,
-  //          18, 5, 13,
-
-  //          // 16, 17, 11, 12, 9
-  //          16, 9, 17,
-  //          16, 11, 9,
-  //          17, 9, 12,
-
-  //          // 15, 16, 10, 11, 8
-  //          15, 8, 16,
-  //          15, 10, 8,
-  //          16, 8, 11
-  //      };
-
-        private static readonly ushort[] dodecahedron_faceIndexes_flipped = {
+        // each group of 3 defines a triangle
+        // each group of 3 triangles defines a pentagon
+        // there are 12 pentagons altogether defining a dodecahedron
+        private static readonly ushort[] dodecahedronFaceIndexes = {
             // one of pair of opposite pentagons parallel to xy plane
             // 0, 1, 2, 3, 4
             1, 3, 0,
@@ -169,7 +109,7 @@ namespace Dodecahedroid
             0, 5, 13,
             1, 13, 6,
 
-
+            // the other one of pair of opposite pentagons parallel to xy plane?
             // 15, 16, 17, 18, 19
             15, 18, 16,
             15, 19, 18,
@@ -232,31 +172,33 @@ namespace Dodecahedroid
         {
             List<float> vertexList = new List<float>();
 
-            float factor = 1f / (1f - 0.382f);
+            //float factor = 1f / (1f - 0.382f);
             //float factor = 1.618f;
             //float factor = 1f;
 
             int pentagonSegment = 0;
 
-            for (int i=0; i<dodecahedron_faceIndexes_flipped.Length; i+=3)
+            for (int i = 0; i < dodecahedronFaceIndexes.Length; i += 3)
             {
-                // calc normal
-                int i1 = dodecahedron_faceIndexes_flipped[i] * 3;
-                int i2 = dodecahedron_faceIndexes_flipped[i+1] * 3;
-                int i3 = dodecahedron_faceIndexes_flipped[i+2] * 3;
-                
-                float ax = dodecahedron_vertices[i1] * factor;
-                float ay = dodecahedron_vertices[i1+1] * factor;
-                float az = dodecahedron_vertices[i1+2] * factor;
 
-                float bx = dodecahedron_vertices[i2] * factor;
-                float by = dodecahedron_vertices[i2+1] * factor;
-                float bz = dodecahedron_vertices[i2+2] * factor;
+                int i1 = dodecahedronFaceIndexes[i] * 3;
+                int i2 = dodecahedronFaceIndexes[i + 1] * 3;
+                int i3 = dodecahedronFaceIndexes[i + 2] * 3;
 
-                float cx = dodecahedron_vertices[i3] * factor;
-                float cy = dodecahedron_vertices[i3+1] * factor;
-                float cz = dodecahedron_vertices[i3+2] * factor;
+                // get the vertices for this triangular pentagon segement
+                float ax = explicitDodecahedronVertices[i1];
+                float ay = explicitDodecahedronVertices[i1 + 1];
+                float az = explicitDodecahedronVertices[i1 + 2];
 
+                float bx = explicitDodecahedronVertices[i2];
+                float by = explicitDodecahedronVertices[i2 + 1];
+                float bz = explicitDodecahedronVertices[i2 + 2];
+
+                float cx = explicitDodecahedronVertices[i3];
+                float cy = explicitDodecahedronVertices[i3 + 1];
+                float cz = explicitDodecahedronVertices[i3 + 2];
+
+                // calc normals
                 float ux = ax - bx;
                 float uy = ay - by;
                 float uz = az - bz;
@@ -269,27 +211,28 @@ namespace Dodecahedroid
                 float ny = (uz * vx) - (ux * vz);
                 float nz = (ux * vy) - (uy * vx);
 
+                // get the texture coordinates
+                // todo: lookup table
                 float texa_x, texa_y, texb_x, texb_y, texc_x, texc_y;
-
-                switch(pentagonSegment)
+                switch (pentagonSegment)
                 {
-                    case 0:    
-                        texa_x = 1-0.7847f;
-                        texa_y = 1-0.0639f;
-                        texb_x = 1-0.5f;
-                        texb_y = 1-0.9375f;
-                        texc_x = 1-0.2167f;
-                        texc_y = 1-0.0639f;
+                    case 0:
+                        texa_x = 1 - 0.7847f;
+                        texa_y = 1 - 0.0639f;
+                        texb_x = 1 - 0.5f;
+                        texb_y = 1 - 0.9375f;
+                        texc_x = 1 - 0.2167f;
+                        texc_y = 1 - 0.0639f;
                         pentagonSegment = 1;
                         break;
 
                     case 1:
-                        texa_x = 1-0.7847f;
-                        texa_y = 1-0.0639f;
-                        texb_x = 1-0.9597f;
-                        texb_y = 1-0.6056f;
-                        texc_x = 1-0.5f;
-                        texc_y = 1-0.9375f;
+                        texa_x = 1 - 0.7847f;
+                        texa_y = 1 - 0.0639f;
+                        texb_x = 1 - 0.9597f;
+                        texb_y = 1 - 0.6056f;
+                        texc_x = 1 - 0.5f;
+                        texc_y = 1 - 0.9375f;
                         pentagonSegment = 2;
                         break;
 
@@ -321,30 +264,28 @@ namespace Dodecahedroid
             }
 
             vertices = vertexList.ToArray();
-            vertices = ApplyContractionMappings(vertices);
-            vertices = ApplyContractionMappings(vertices);
-            vertices = ApplyContractionMappings(vertices);
+            vertices = ApplyContractionMappings(vertices, 3);
 
 
             faceIndexes = new uint[vertices.Length];
-            for(uint i=0; i< faceIndexes.Length; i++)
+            for (uint i = 0; i < faceIndexes.Length; i++)
             {
                 faceIndexes[i] = i;
             }
         }
 
-        private static float[] ApplyContractionMappings(float[] vertices)
+        private static float[] ApplyContractionMappings(float[] vertices, int iterations)
         {
             List<float> vertexList = new List<float>();
 
             for (int m = 0; m < contractionMappings.GetLength(0); m++)
             {
-                for (int n = 0; n < vertices.Length; n+=8)
+                for (int n = 0; n < vertices.Length; n += 8)
                 {
                     float x = vertices[n];
-                    float y = vertices[n+1];
-                    float z = vertices[n+2];
-                    
+                    float y = vertices[n + 1];
+                    float z = vertices[n + 2];
+
                     float a = contractionMappings[m, 0];
                     float b = contractionMappings[m, 1];
                     float c = contractionMappings[m, 2];
@@ -362,15 +303,14 @@ namespace Dodecahedroid
                     float ydash = d * x + e * y + f * z + k;
                     float zdash = g * x + h * y + i * z + l;
 
-                    vertexList.AddRange(new List<float>() { xdash, ydash, zdash, vertices[n+3], vertices[n + 4], vertices[n + 5], vertices[n + 6], vertices[n + 7] });
+                    vertexList.AddRange(new List<float>() { xdash, ydash, zdash, vertices[n + 3], vertices[n + 4], vertices[n + 5], vertices[n + 6], vertices[n + 7] });
                 }
             }
-            return vertexList.ToArray();
+            var vertexArray = vertexList.ToArray();
+            return iterations > 1 ? ApplyContractionMappings(vertexArray, iterations - 1) : vertexArray;
         }
 
         internal static float[] vertices;
         internal static uint[] faceIndexes;
-	}
-
-    
+    }
 }
